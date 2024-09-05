da9196ef278695156d4269e4816578c7477bd5c1
Alon Yakoby <alon.yakoby@gmail.com>
2024-09-05 10:08:20 +0200

    Maintnance Header

--- src/components/ImageHeader.js
+++ src/components/ImageHeader.js
@@ -9,13 +9,19 @@ const ImageHeader = ({ className = "" }) => {
         <div className={styles.winBigPrizesAsPartOfBundParent}>
           <div className={styles.winBigPrizesContainer}>
             <p className={styles.winBigPrizes}>
-              Team Login Verzögerung,
+              Saisonstart
+            </p>
+            <p className={styles.toCelebrateBundesliga}>
+              Wichtiges update bis 8.9.2024,
+            </p>
+            <p className={styles.toCelebrateBundesliga}>
+              Liebe ÖKFB Mitglieder,
             </p>
           </div>
           <div className={styles.toCelebrateBundesliga}>
-            Liebe ÖKFB Mitglieder,
-            Aufgrund der großen Umstellungen kommt es zu leichten Verzögerungen. Die Login Daten der Teams werden im Laufe der Woche an die angegebene E-Mail Adresse versendet. Falls ihr diese Woche keine Login Daten bekommt, bitten wir euch uns eine Mail an support@oekfb.eu zu senden.
-            Vielen Dank für euer Verständnis.          </div>
+            wir sind ständig dabei, unsere Dienstleistungen für Sie zu verbessern. Deshalb freuen wir uns, Ihnen mitteilen zu können, dass wir ein neues Update durchführen. Dieses Update umfasst neben der Ergänzung von Statistiken und neuen Funktionen zum Ausfüllen des Blankett auch wichtige Fehlerbehebungen. Es wird ab Sonntag, dem 8.9., online verfügbar sein.
+            Während wir an diesem Update arbeiten, danken wir Ihnen herzlich für Ihr Verständnis und Ihre Geduld. Bei dringenden Anfragen oder Fragen können Sie sich gerne an unser Büro wenden oder uns unter support@oekfb.eu kontaktieren.
+            Vielen Dank und eine erfolgreiche Saison!          </div>
         </div>
       </div>
     </div>
