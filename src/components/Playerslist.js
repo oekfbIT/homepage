3eb6144b71dfdea382cc6112ca62d4899998656b
Alon Yakoby <alon.yakoby@gmail.com>
2024-09-09 14:15:59 +0200

    PLayer sorter

--- src/components/Playerslist.js
+++ src/components/Playerslist.js
@@ -3,19 +3,32 @@ import PlayerCell from "./PlayerCell";
 import styles from "./Playerslist.module.css";
 
 const Playerslist = ({ className = "", players }) => {
+  // Function to sort players based on eligibility
+  const sortByEligibility = (a, b) => {
+    const eligibilityOrder = {
+      "Spielberechtigt": 1,
+      "Warten": 2,
+      "Gesperrt": 3,
+    };
+
+    return (eligibilityOrder[a.eligibility] || 4) - (eligibilityOrder[b.eligibility] || 4);
+  };
+
   return (
       <div className={[styles.playerslist, className].join(" ")}>
-        {players.map(player => (
-            <PlayerCell
-                key={player.id}
-                name={player.name}
-                nationality={player.nationality}
-                birthday={player.birthday}
-                position={player.position}
-                image={player.image}
-                eligibility={player.eligibility}
-            />
-        ))}
+        {players
+            .sort(sortByEligibility)
+            .map(player => (
+                <PlayerCell
+                    key={player.id}
+                    name={player.name}
+                    nationality={player.nationality}
+                    birthday={player.birthday}
+                    position={player.position}
+                    image={player.image}
+                    eligibility={player.eligibility}
+                />
+            ))}
       </div>
   );
 };
@@ -29,7 +42,7 @@ Playerslist.propTypes = {
     birthday: PropTypes.string.isRequired,
     position: PropTypes.string.isRequired,
     image: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
+    eligibility: PropTypes.string.isRequired,
   })).isRequired,
 };
 
